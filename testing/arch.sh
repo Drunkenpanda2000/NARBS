6ca1f8e2939981c2fc247e5d9bc8810b9bf1e5af
Nicolaas  Tanghe <nic.tanghe@gmail.com>
2018-09-07 20:15:34 +0200

    first commit

---
+++ testing/arch.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#warning!  this wil format dev/sda
+
+pacman -S --noconfirm dialog || { echo "Error at script start: Are you sure you're running this as the root user? Are you sure you have an internet connection?"; exit; }
+
+dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
+
+dialog --defaultno --title "Time Zone select" --yesno "Do you want use the default time zone(Europe/Brussels)?.\n\nPress no for select your own time zone"  10 60 && echo "Europe/Brussels" > tz.tmp || tzselect > tz.tmp
+
+dialog --no-cancel --inputbox "Enter partitionsize in gb, separated by space (swap & root)." 10 60 2>psize
+
+IFS=' ' read -ra SIZE <<< $(cat psize)
+
+re='^[0-9]+$'
+if ! [ ${#SIZE[@]} -eq 2 ] || ! [[ ${SIZE[0]} =~ $re ]] || ! [[ ${SIZE[1]} =~ $re ]] ; then
+    SIZE=(12 25);
+fi
+
+timedatectl set-ntp true
+
+cat <<EOF | fdisk /dev/sda
+o
+n
+p
+
+
++200M
+n
+p
+
+
++${SIZE[0]}G
+n
+p
+
+
++${SIZE[1]}G
+n
+p
+
+
+w
+EOF
+partprobe
+
+yes | mkfs.ext4 /dev/sda4
+yes | mkfs.ext4 /dev/sda3
+yes | mkfs.ext4 /dev/sda1
+mkswap /dev/sda2
+swapon /dev/sda2
+mount /dev/sda3 /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir -p /mnt/home
+mount /dev/sda4 /mnt/home
+
+
+pacstrap /mnt base base-devel
+
+genfstab -U /mnt >> /mnt/etc/fstab
+cat tz.tmp > /mnt/tzfinal.tmp
+rm tz.tmp
+mv comp /mnt/etc/hostname
+curl https://raw.githubusercontent.com/Drunkenpanda2000/NARBS/master/testing/chroot.sh > /mnt/chroot.sh && arch-chroot /mnt bash chroot.sh && rm /mnt/chroot.sh
+
+
+dialog --defaultno --title "Final Qs" --yesno "Reboot computer?"  5 30 && reboot
+dialog --defaultno --title "Final Qs" --yesno "Return to chroot environment?"  6 30 && arch-chroot /mnt
+clear
---
+++ testing/chroot.sh
@@ -0,0 +1,25 @@
+#Potential variables: timezone, lang and local
+
+passwd
+
+TZuser=$(cat tzfinal.tmp)
+
+ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
+
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US ISO-8859-1" >> /etc/locale.gen
+locale-gen
+
+echo "KEYMAP=be-latin1" >> /etc/vconsole.conf
+
+pacman --noconfirm --needed -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
+pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman --noconfirm --needed -S dialog
+larbs() { curl -O https://raw.githubusercontent.com/drunkenpanda2000/LARBS/master/narbs.sh && bash narbs.sh ;}
+dialog --title "Install ArchRice" --yesno "This install script will easily let you access Nic's Auto-Rice Boostrapping Scripts (NARBS) which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\nNic"  15 60 && narbs
